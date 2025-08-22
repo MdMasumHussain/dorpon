caefbbda15b89c05b5979e823cdaec02dc153688
Md Masum Hossain <97906106+MdMasumHussain@users.noreply.github.com>
2025-08-22 19:08:40 +0600

    Update carousel.js

--- dorponFrontend/src/app/lib/carousel.js
+++ dorponFrontend/src/app/lib/carousel.js
@@ -1,5 +1,5 @@
 export async function faceCarousel() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_USER_URL}/carousel`)
+    const res = await fetch(`/api/carousel`)
     const carousel = await res.json()
     return carousel;
 }
